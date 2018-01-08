66eea6a676e596cb0dc223e75a743e9c7203429f
Fedor Chervyakov <theodor.chervyakov.timofeevich@gmail.com>
2018-01-09 02:37:13 +0300

    Small stability improvements

--- telegram_bot.py
+++ telegram_bot.py
@@ -23,6 +23,7 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, ConversationHandler
 from telegram.ext import Filters, RegexHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram import ParseMode
 
 from time import time, sleep
 from datetime import datetime, timedelta
@@ -46,17 +47,14 @@ _format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=_format,level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-fh = RotatingFileHandler('telegram_bot.log',maxBytes=16384)
+fh = RotatingFileHandler('telegram_bot.log',maxBytes=8192)
 fh.setLevel(logging.INFO)
 fh.setFormatter(logging.Formatter(_format))
 logger.addHandler(fh)
 
-sh = logging.StreamHandler()
-sh.setLevel(logging.INFO)
-sh.setFormatter(logging.Formatter(_format))
-logger.addHandler(sh)
-
-
+'''
+    SOME USEFUL CONSTANTS
+'''
 START_TEXT = ('To upload a file, send it here and then follow my instructions. Only admins can schedule file uploads! '
              +'To ask a question start by sending /topics command') 
 DOWNLOAD_FILE_TEXT = ('You have uploaded file to the server. Your next step is ' 
@@ -64,21 +62,27 @@ DOWNLOAD_FILE_TEXT = ('You have uploaded file to the server. Your next step is '
                     + 'hh:mm dd Month yyyy')
 DATE_FORMAT_ERR_TEXT = 'You are using incorrect formatting. TRY AGAIN. Your reply should look like this: 4:20 26 December 2017'
 
-
-with open('.secret/TOKEN','r+') as f:
+# Reading token from secret file
+with open('.secret/TOKEN','r') as f:
     TOKEN = f.read()[:-1]
 
+# Channel related constants
 CHANNEL_ID = -1001100253926
 CHANNEL_INVITE_LINK = 'https://t.me/joinchat/AAAAAEGUiubGLELh-MwPWA'
 
+# List of users who can schedule uploads in the channel
 LIST_OF_ADMINS = [219630622,392783281]
 
+# Paths to various data
 CONVERSATIONS_PATH = os.path.join('backup','conversations')
 USERDATA_PATH = os.path.join('backup','user_data')
 JOBS_PICKLE = os.path.join('backup','job_tuples.pickle')
 DATABASE_PATH = os.path.join('qa_data','questions.db')
 
+# ConversationHandler states
+# This one is for file upload and scheduling
 SCHEDULE = range(1)
+# These ones are for question search 
 SELECT_TOPIC, FIND_QUESTIONS, SELECT_QUESTION = range(3)
 
 MONTHS = {'January': 1, 'February': 2, 'March': 3,'April': 4,'May': 5,'June': 6,
@@ -88,24 +92,25 @@ date_pattern = re.compile('^(\d\d?):(\d\d?) (\d\d?) (January|February|March'
                             + '|April|May|June|July|August|September'
                             + '|October|November|December) (\d\d\d\d)$') 
 
-QUESTIONS_KEYBOARD = [['1','2','3'],['4','5','6'],
+TOPICS_KEYBOARD = [['1','2','3'],['4','5','6'],
                       ['7','8','9'],['10','Cancel']]
 
 db = Database(DATABASE_PATH)
 
 TOPIC_NAMES = db.get_unique_topics()
 
-logger.info(TOKEN)
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
 job_queue = updater.job_queue
 
 def restricted(func):
+    ''' Wrapper that restricts access to specified function.
+    '''
     @wraps(func)
     def wrapped(bot,update,*args,**kwargs):
         user_id = update.effective_user.id
         if user_id not in LIST_OF_ADMINS:
-            self.logger.warning("Unauthorized access denied for {}.".format(user_id))
+            logger.warning("Unauthorized access denied for {0}.".format(user_id))
             return
         return func(bot, update, *args, **kwargs)
     return wrapped
@@ -115,20 +120,30 @@ def start(bot, update):
 
 @restricted
 def download_file(bot, update, user_data):
-    file_id = update.message.document.file_id
-    file_name = update.message.document.file_name
+    ''' Saves file on the hard drive.
+    '''    
+    if update.message.photo:
+        photo = update.message.photo[-1]
+        file_id = photo['file_id']
+        user_data['file_name'] = 'photo' 
+    elif update.message.document:
+        file_name = update.message.document.file_name
+        file_id = update.message.document.file_id
+        user_data['file_name'] = file_name
     newFile = bot.get_file(file_id)
     newFile.download(custom_path=(os.path.join('downloaded_data',file_name)))
-    user_data['file_name'] = file_name
     user_data['file_id'] = file_id
     logger.info('File %s downloaded.' % file_id)
-    logger.debug('Original filename is %s' % file_name)
     bot.send_message(chat_id=update.message.chat_id,text=DOWNLOAD_FILE_TEXT)
     return SCHEDULE
 
 def publish_file(bot,job):
-    logger.info('publish file job context: %s' % job.context)
-    bot.send_document(chat_id=CHANNEL_ID, document=job.context)
+    logger.debug('Publish file job context: %s' % job.context)
+    logger.info('Sending file {0} to the channel.'.format(job.context))
+    try:
+        bot.send_document(chat_id=CHANNEL_ID, document=job.context)
+    except:
+        bot.send_photo(chat_id=CHANNEL_ID,photo=open(os.path.join('downloaded_data',job.context),'rb'))
 
 @restricted
 def schedule(bot,update,job_queue,user_data):
@@ -145,12 +160,12 @@ def schedule(bot,update,job_queue,user_data):
         update.message.reply_text(DATE_FORMAT_ERR_TEXT)
         return SCHEDULE
 
-def topics(bot,update,user_data):
+def topics(bot,update):
     reply = 'Please select a topic by typing corresponding number.\n'
     n = range(1,len(TOPIC_NAMES) + 1)
     reply_list = list(map(lambda i,t: '{0}. {1}'.format(i,t),n,TOPIC_NAMES))
     reply += '\n'.join(reply_list)
-    reply_markup = ReplyKeyboardMarkup(QUESTIONS_KEYBOARD)
+    reply_markup = ReplyKeyboardMarkup(TOPICS_KEYBOARD)
     bot.send_message(chat_id=update.message.chat_id,text=reply,
                              reply_markup=reply_markup)
     return SELECT_TOPIC
@@ -165,8 +180,10 @@ def find_questions(bot,update,user_data):
             selected_topic = TOPIC_NAMES[choice-1]
             logger.info('User {0} selected topic {1}'.format(update.effective_user.id,selected_topic))
             user_data['topic'] = selected_topic
-            reply = 'You selected topic {0}.\nNext, please type in your search query'.format(selected_topic)
-            bot.send_message(chat_id=update.message.chat_id,text=reply,reply_markup=ReplyKeyboardRemove())
+
+            reply = 'You selected topic <b>{0}</b>.\nNext, please type in your search query'.format(selected_topic)
+            bot.send_message(chat_id=update.message.chat_id,text=reply,
+                            reply_markup=ReplyKeyboardRemove(), parse_mode=ParseMode.HTML)
             return FIND_QUESTIONS
         else:
             bot.send_message(chat_id=update.message.chat_id,text='Invalid number, try again!')
@@ -174,13 +191,15 @@ def find_questions(bot,update,user_data):
     else:
         bot.send_message(chat_id=update.message.chat_id,text='Please enter a number!')
         return SELECT_TOPIC
- 
+
+#BTW naming here is fucked up 
 def show_questions(bot,update,user_data):
     text = update.message.text
     logger.debug(text)
     questions = db.find_questions(user_data['topic'],text)
     if not questions:
-        bot.send_message(chat_id=update.message.chat_id,text='Your search did not match any questions. Try again!')
+        bot.send_message(chat_id=update.message.chat_id,
+                        text='Your search did not match any questions. Try again!')
         return FIND_QUESTIONS
     
     logger.info('User {0} requested {1}'.format(update.effective_user.id,text))
@@ -189,9 +208,11 @@ def show_questions(bot,update,user_data):
     for i in range(1,len(q)+1):
         q[i-1] = '{0}. {1}'.format(i,q[i-1])
     qs = '\n'.join(q)
-    reply = ('Topic: {0}\nSelect a question by typing' 
-            + 'corresponding number.\nQuestions:\n{1}').format(user_data['topic'], qs)
-    bot.send_message(chat_id=update.message.chat_id,text=reply,reply_markup=ReplyKeyboardRemove())
+    reply = ('<b>Topic:</b> {0}\nSelect a question by typing ' 
+            + 'corresponding number.\n<b>Questions:</b>\n{1}').format(user_data['topic'], qs)
+    bot.send_message(chat_id=update.message.chat_id,text=reply,
+                    reply_markup=ReplyKeyboardRemove(),
+                    parse_mode=ParseMode.HTML)
     return SELECT_QUESTION
 
 def show_answer(bot,update,user_data):
@@ -208,9 +229,10 @@ def show_answer(bot,update,user_data):
             question_id = questions[choice-1][1]
             answer = db.find_answers(question_id)
             a = '\n'.join(answer)
-            reply = 'Topic: {0}\nQ: {1}\nA: {2}'.format(topic,question,a)
+            reply = '<b>Topic:</b> {0}\n<b>Q:</b> {1}\n<b>A:</b> {2}'.format(topic,question,a)
             bot.send_message(chat_id=update.message.chat_id,
-                            text=reply,reply_markup=ReplyKeyboardRemove())
+                            text=reply,reply_markup=ReplyKeyboardRemove(),
+                            parse_mode=ParseMode.HTML)
         else:
             update.message.reply_text('Invalid number, try again!')
             return SELECT_QUESTION
@@ -305,18 +327,27 @@ def saveData(conv_handler):
             with open(USERDATA_PATH, 'wb+') as f:
                 pickle.dump(dispatcher.user_data, f)
         except:
-            logging.exception(sys.exc_info()[0])
+            logger.exception(sys.exc_info()[0])
+
+def stop_and_restart():
+    updater.stop()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+def restart(bot,update):
+    update.message.reply_text('Bot is restarting...')
+    logger.info('Bot is restarting.')
+    Thread(target=stop_and_restart).start()
 
 def error(bot,update,error):
-    logger.warning('Update "%s" caused error "%s"',update,error)
+    logger.exception('Update "%s" caused error "%s"',update,error)
 
 def main():
     
-
     start_handler = CommandHandler('start',start)
     channel_handler = CommandHandler('channel',channel)
-
-    download_handler = MessageHandler(Filters.document, download_file, pass_user_data=True)
+    
+    # Handling file schedule conversation below
+    download_handler = MessageHandler(Filters.document | Filters.photo, download_file, pass_user_data=True)
     schedule_handler = MessageHandler(Filters.text,schedule,pass_job_queue=True,pass_user_data=True)
     upload_handler = ConversationHandler(
             entry_points=[download_handler],
@@ -324,7 +355,8 @@ def main():
                 SCHEDULE : [schedule_handler]},
             fallbacks=[CommandHandler('cancel',fb,pass_user_data=True)])
     
-    topics_handler = CommandHandler('topics',topics,pass_user_data=True)
+    # Handling question search here
+    topics_handler = CommandHandler('topics',topics)
     qa_handler = ConversationHandler(
             entry_points=[topics_handler],
             states={
@@ -333,20 +365,14 @@ def main():
                 SELECT_QUESTION : [MessageHandler(Filters.text, show_answer, pass_user_data=True)]},
             fallbacks=[CommandHandler('cancel',fb,pass_user_data=True)])
     
+    # Loading data from handler backups
+    # and starting saveData threads.
     loadData(upload_handler)
     loadData(qa_handler)
     thrd1 = Thread(target=saveData,args=[qa_handler]).start()
     thrd2 = Thread(target=saveData,args=[upload_handler]).start()
 
-    def stop_and_restart():
-        updater.stop()
-        os.execl(sys.executable, sys.executable, *sys.argv)
-
-    def restart(bot,update):
-        update.message.reply_text('Bot is restarting...')
-        logger.info('Bot is restarting.')
-        Thread(target=stop_and_restart).start()
-    
+    # Adding message handlers specified above to dispatcher 
     dispatcher.add_handler(CommandHandler('r',restart,filters=Filters.user(username='@theodor3k')))
     dispatcher.add_handler(qa_handler)
     dispatcher.add_handler(start_handler)
@@ -364,6 +390,7 @@ def main():
     updater.start_polling()
     updater.idle()
     
+    # Saving jobs after proper shutdown.
     save_jobs(job_queue)
 
 if __name__ == '__main__':
